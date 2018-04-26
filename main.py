f7a8cab828d781c4a6390fd77103ece4b180aba2
Pavel Grechanuk <grechanp@oregonstate.edu>
2018-04-26 10:03:02 -0700

    added gaussian quadrature

--- main.py
+++ main.py
@@ -5,6 +5,29 @@ import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import integrate
+
 
 grid = pd.read_csv('grid.csv')
 xs = pd.read_csv('XS.csv', index_col = 'material')
+
+
+# Define function and interval
+a = 0.0
+b = np.pi/2
+f = lambda x: np.cos(x)
+
+# Gauss-Legendre (default interval is [-1, 1])
+deg = 10
+x, w = np.polynomial.legendre.leggauss(deg)
+# Translate x values from the interval [-1, 1] to [a, b]
+t = 0.5*(x + 1)*(b - a) + a
+gauss = sum(w * f(t)) * 0.5*(b - a)
+
+# For comparison
+quad, quad_err = integrate.quad(f, a, b)
+
+print(quad, quad_err)
+print(gauss)
+print(gauss - quad)
+
