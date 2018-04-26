552beb35ad8d2c468e51db4021936bff71d9e3e3
grechasneak <grechanp@oregonstate.edu>
2018-04-26 10:20:14 -0700

    put the gaussian quad into a function

--- main.py
+++ main.py
@@ -12,31 +12,37 @@ grid = pd.read_csv('grid.csv')
 xs = pd.read_csv('XS.csv', index_col = 'material')
 
 
-<<<<<<< HEAD
 
-material = grid.iloc[1, 1]
+material = grid.iloc[0, 1]
 sigma_t1 = xs.loc[material, 'sigma_t1']
 sigma_s11 = xs.loc[material, 'sigma_s11']
 sigma_t2 = xs.loc[material, 'sigma_t2']
 sigma_s22 = xs.loc[material, 'sigma_s22']
-=======
-# Define function and interval
-a = 0.0
-b = np.pi/2
+
 f = lambda x: np.cos(x)
+a = 0.0
+b = np.pi/2	
+	
+def gaussian_integration(f, a, b, degree):
+	# Gauss-Legendre (default interval is [-1, 1])
+	degree = 10
+	x, w = np.polynomial.legendre.leggauss(degree)
+	# Translate x values from the interval [-1, 1] to [a, b]
+	t = 0.5*(x + 1)*(b - a) + a
+	gauss = sum(w * f(t)) * 0.5*(b - a)
+	return gauss
 
-# Gauss-Legendre (default interval is [-1, 1])
-deg = 10
-x, w = np.polynomial.legendre.leggauss(deg)
-# Translate x values from the interval [-1, 1] to [a, b]
-t = 0.5*(x + 1)*(b - a) + a
-gauss = sum(w * f(t)) * 0.5*(b - a)
+	
+	
+def step_characteristic():
+	pass
 
+	
+	
 # For comparison
 quad, quad_err = integrate.quad(f, a, b)
-
+gauss = gaussian_integration(f, a, b, 10)
 print(quad, quad_err)
 print(gauss)
 print(gauss - quad)
 
->>>>>>> f7a8cab828d781c4a6390fd77103ece4b180aba2
