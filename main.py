9f27e992d203fa5e21c6150e251748f79c9b6446
Pavel Grechanuk <grechanp@oregonstate.edu>
2018-05-01 14:56:22 -0700

    fixing bugs

--- main.py
+++ main.py
@@ -18,13 +18,6 @@ class Step_characteristic():
 		self.fluxes = [np.ones(len(grid)), np.ones(len(grid))]
 		self.currents = [np.ones(len(grid)), np.ones(len(grid))]
 
-	def gen_fissionSource(self): 
-		fission_sourceF = []
-		for material in self.grid['material']:
-			nu2 = self.xs.loc[material, 'nu2']
-			sigma_f2 = self.xs.loc[material, 'sigma_f2']
-			fission_sourceF.append(nu2 * sigma_f2)
-		return np.asarray(fission_sourceF) * self.fluxes[1]	
 		
 	def update_Source(self):
 		self.source = [np.zeros(len(self.grid)), np.zeros(len(self.grid))]
@@ -45,82 +38,80 @@ class Step_characteristic():
 			
 			
 	def iterate_flux(self):
-		self.update_Source()
+
 		fluxes_ = [np.zeros(len(grid)), np.zeros(len(grid))]
 		currents_ = [np.zeros(len(grid)), np.zeros(len(grid))]
 		
-		for group in range(len(fluxes)):
+		current_in = 0.005
+		for group in range(len(self.fluxes)):
 		
-			for idx, mu in enumerate(self.directions)):
+			for idx, mu in enumerate(self.directions):
 				if mu > 0:
 					for i in self.cells:
 						material = grid['material'].iloc[i]
 						sigma_t1 = xs.loc[material, 'sigma_t1']
 						sigma_t2 = xs.loc[material, 'sigma_t2']
+						xs_data = [sigma_t1, sigma_t2]
 						
-						tau_1 = spacing * sigma_t1 / mu
-						tau_2 = spacing * sigma_t2 / mu
+						tau = spacing * xs_data[group] / abs(mu)
+						Q_cell = self.source[group][i]
+
+						current_out = current_in * np.exp(-tau) + Q_cell / xs_data[group] * (1 - np.exp(-tau))
+						current_cell = Q_cell / xs_data[group] - mu * (current_out - current_in)/(spacing * xs_data[group])
+						
+						fluxes_[group][i] += current_cell * self.weights[idx]
+						currents_[group][i] += current_out * self.weights[idx] * self.directions[idx]
+
+						current_in = current_out
 					
-					# current_out = current_in * np.exp(-tau) + Q_cell / sigma_t * (1 - np.exp(-tau))
-					# current_mean = calc_mean_current(current_in, current_out, Q, directions[idx], sigma_t1)
-					
-					# flux[i] += current_mean * weights[idx]
-					# current_cell[i] = current_mean * weights[idx] * directions[idx]
-					
-					# current_in = current_out
-				else:	
-					for i in reversed(cells):
+	
+						
+				if mu < 0:
+					for i in reversed(self.cells):
 						material = grid['material'].iloc[i]
-					sigma_t1 = xs.loc[material, 'sigma_t1']
-					# tau = spacing * sigma_t1 / directions[idx]
-					
-					# current_out = calc_current_out(current_in, Q, tau, sigma_t1)
-					# current_mean = calc_mean_current(current_in, current_out, Q, directions[idx], sigma_t1)
-					
-					# flux[i] += current_mean * weights[idx]
-					# current_cell[i] += current_mean * weights[idx] * directions[idx]
-					# print('left')
-					# current_in = current_out
+						sigma_t1 = xs.loc[material, 'sigma_t1']
+						sigma_t2 = xs.loc[material, 'sigma_t2']
+						xs_data = [sigma_t1, sigma_t2]
+						
+						tau = spacing * xs_data[group] / abs(mu)
+						Q_cell = self.source[group][i]
+						
+						current_out = current_in * np.exp(-tau) + Q_cell / xs_data[group] * (1 - np.exp(-tau))
+						current_cell = Q_cell / xs_data[group] - mu * (current_out - current_in)/(spacing * xs_data[group])
+						
+						fluxes_[group][i] += current_cell * self.weights[idx]
+						currents_[group][i] += current_out * self.weights[idx] * self.directions[idx]
+						
+						current_in = current_out
+
+	
+		
+			self.fluxes[group] = fluxes_[group]			
+			self.currents[group] = currents_[group]	
 		
 
 	
-	def calc_current_out(current_in, Q_cell, tau, sigma_t):
-		return current_in * np.exp(-tau) + Q_cell / sigma_t * (1 - np.exp(-tau))
-	
-	def calc_mean_current(current_in, current_out, Q_cell, mu, sigma_t):
-		return Q_cell / sigma_t - mu * (current_out - current_in)/(spacing * sigma_t)
-	
-
-	
 
 
-def gen_fissionSource(grid, xs, flux): 
-	fission_sourceF = []
-	for material in grid['material']:
-		nu2 = xs.loc[material, 'nu2']
-		sigma_f2 = xs.loc[material, 'sigma_f2']
-		fission_sourceF.append(nu2 * sigma_f2)
-	return np.asarray(fission_sourceF) * flux
+	
 	
 grid = pd.read_csv('grid.csv')
 xs = pd.read_csv('XS.csv', index_col = 'material')
 
 global spacing
 spacing = 0.15625
-order = 20
-directions, weights = np.polynomial.legendre.leggauss(order)
+order = 6
 
 
 
-Q = 1/100
-cells = range(128)
 
-current_in = 0.001
 
-flux = np.zeros(128)
-current_cell = np.zeros(128)
+flux_solver = Step_characteristic(grid, xs, 6)
+for i in range(10):
+	flux_solver.update_Source()
+	for i in range(10):
+		flux_solver.iterate_flux()
 
-flux_solver = Step_characteristic(grid, xs, 20)
 
 # for i in range(100):
 	# for idx in range(len(directions)):
