af574a8aa6c500307c4715d1c2d9f308b05b5e69
Pavel Grechanuk <grechanp@oregonstate.edu>
2018-05-04 16:08:23 -0700

    solution converges and power iteration works

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ class SnTransportSolver():
 		
 		self.fluxes = [np.ones(len(grid)), np.ones(len(grid))] #initial flux is uniform
 		self.currents = [np.ones(len(grid)), np.ones(len(grid))]
-		self.k = 1.0
+		self.k = 1.5
 		self.update_Source()
+		self.converged = False
 
 		
 	def update_Source(self):
@@ -35,27 +36,26 @@ class SnTransportSolver():
 			if material != 'water': #only thermal fission
 				nu2 = self.xs.loc[material, 'nu2']
 				sigma_f2 = self.xs.loc[material, 'sigma_f2']
-				self.fission_source[0][i] += self.fluxes[1][i] * nu2 * sigma_f2 / (self.k * 2)
+				self.fission_source[0][i] += self.fluxes[1][i] * nu2 * sigma_f2 
 				
-			self.scatter_source[0][i] += (self.fluxes[0][i] * sigma_s11) / 2#within group fast scatter
-			self.scatter_source[1][i] += (self.fluxes[0][i] * sigma_s12 + self.fluxes[1][i] * sigma_s22) / 2 #fast down scatter thermal within scatter
+			self.scatter_source[0][i] += (self.fluxes[0][i] * sigma_s11) #within group fast scatter
+			self.scatter_source[1][i] += (self.fluxes[0][i] * sigma_s12 + self.fluxes[1][i] * sigma_s22)  #fast down scatter thermal within scatter
 			
 		self.scatter_source = np.asarray(self.scatter_source)
 		self.fission_source = np.asarray(self.fission_source) 
 			
-		self.scatter_source[0] = self.scatter_source[0]/max(self.scatter_source[0])
-		self.scatter_source[1] = self.scatter_source[1]/max(self.scatter_source[1])
+		#self.scatter_source[0] = self.scatter_source[0]/max(self.scatter_source[0])
+		#self.scatter_source[1] = self.scatter_source[1]/max(self.scatter_source[1])
 		
-		self.fission_source[0] = self.fission_source[0]/max(self.fission_source[0])
+		#self.fission_source[0] = self.fission_source[0]/max(self.fission_source[0])
 		
 		return self.scatter_source, self.fission_source
 			
 			
 	def iterate_flux(self):
 		percent_diff = 100
-		current_in = 0.01
 		
-		boundary_currents = [np.ones(len(self.mu)), np.ones(len(self.mu))]
+		boundary_currents = [np.zeros(len(self.directions)), np.zeros(len(self.directions))]
 		
 		while percent_diff > self.stopping_criteria: #stopping criteria
 			fluxes_ = [np.zeros(len(grid)), np.zeros(len(grid))]
@@ -72,8 +72,9 @@ class SnTransportSolver():
 							xs_data = [sigma_t1, sigma_t2]
 							
 							tau = spacing * xs_data[group] / abs(mu)
-							Q_cell = self.scatter_source[group][i] + self.fission_source[group][i]
-	
+							Q_cell = (self.scatter_source[group][i] + self.fission_source[group][i]/self.k) / 2
+							
+							current_in = boundary_currents[group][idx]
 							
 							current_out = current_in * np.exp(-tau) + Q_cell / xs_data[group] * (1 - np.exp(-tau))
 							current_cell = Q_cell / xs_data[group] - abs(mu) * (current_out - current_in)/(spacing * xs_data[group])
@@ -81,7 +82,7 @@ class SnTransportSolver():
 							fluxes_[group][i] += current_cell * self.weights[idx]
 							currents_[group][i] += current_out * self.weights[idx] * self.directions[idx]
 							
-							current_in = current_out
+							boundary_currents[group][idx] = current_out
 							
 					if mu > 0:
 						for i in self.cells:
@@ -91,15 +92,17 @@ class SnTransportSolver():
 							xs_data = [sigma_t1, sigma_t2]
 							
 							tau = spacing * xs_data[group] / abs(mu)
-							Q_cell =  self.scatter_source[group][i] + self.fission_source[group][i] 
+							Q_cell =  (self.scatter_source[group][i] + self.fission_source[group][i]/self.k) / 2
+							
+							current_in = boundary_currents[group][idx]
 
 							current_out = current_in * np.exp(-tau) + Q_cell / xs_data[group] * (1 - np.exp(-tau))
 							current_cell = Q_cell / xs_data[group] - abs(mu) * (current_out - current_in)/(spacing * xs_data[group])
 							
 							fluxes_[group][i] += current_cell * self.weights[idx]
 							currents_[group][i] += current_out * self.weights[idx] * self.directions[idx]
-
-							current_in = current_out
+							
+							boundary_currents[group][idx] = current_out
 						
 				#stopping criteria
 				percent_diff = np.amax(abs(fluxes_[group] - self.fluxes[group]))
@@ -111,17 +114,17 @@ class SnTransportSolver():
 		source_new, fission_new = self.update_Source()
 		#print(sum(source_new[0]), sum(source_new[1]))
 		
-		k_new = self.k * sum(fission_new[0]/max(fission_new[0])) / sum(fission_old / max(fission_old) )
+		k_new = self.k * sum(fission_new[0]) / sum(fission_old)
+		
 		
 		k_diff = abs(k_new - self.k)/k_new
 		F_diff = abs(np.nanmax((fission_new[0] - fission_old) / fission_new[0]))
-		print(k_new)
-
+		print(k_diff, F_diff)
 		self.k = k_new
-		# if k_diff < self.stopping_criteria and k_diff < self.stopping_criteria:
-			# self.fission_source = fission_old
-		# else:
-			# self.k = k_new
+		if k_diff < self.stopping_criteria and F_diff < self.stopping_criteria:
+			self.converged = True
+			print('converged')
+
 
 
 		
@@ -137,13 +140,15 @@ spacing = 0.15625
 order = 20
 percent_diff = .001
 
-outer_iterations = 10
+outer_iterations = 200
 
 solver = SnTransportSolver(grid, xs, order, percent_diff)
 
-for i in range(outer_iterations):
+while True:
 	solver.iterate_flux()
 	solver.power_iteration()
+	if solver.converged == True:
+		break
 
 
 plt.plot(range(len(solver.fluxes[0])), solver.fluxes[0])
